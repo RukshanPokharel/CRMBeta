181c7f121c43b786343675116984a328a1ccd127
paranshu1234 <paranshu1234@gmail.com>
2022-03-17 03:25:09 +0100

    FIxed navigation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,9 +8,34 @@ import { Component, OnInit } from '@angular/core';
 export class AppComponent implements OnInit {
   public pages = [
     {
-      title: 'Customer',
-      url: '/customer',
+      title: 'Dashboard',
+      url: '/dashboard',
+      icon: 'apps'
+    },
+    {
+      title: 'Customers',
+      url: '/customers',
       icon: 'people',
+    },
+    {
+      title: 'Earnings',
+      url: '/earnings',
+      icon: 'card',
+    },
+    {
+      title: 'Product-Sales',
+      url: '/product-sales',
+      icon: 'analytics',
+    },
+    {
+      title: 'Store-Management',
+      url: '/store-management',
+      icon: 'star-half',
+    },
+    {
+      title: 'Settings',
+      url: '/settings',
+      icon: 'settings',
     },
   ];
   constructor() {}
